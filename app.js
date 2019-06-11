809c396dfb93340faab6914823f3ae44a548628d
Thandolwenkosi Sibanda <sibandathandolwenkosi2@gmail.com>
2019-06-11 09:34:41 +0200

    Save

--- app.js
+++ app.js
@@ -10,28 +10,29 @@ var bodyParser = require('body-parser'),
 
 // =======================================================================================================================================
 //  Include and register all Models
-// 1. User
-// 2. Clients
+//    1. User
+//    2. Clients
 //=========================================================================================================================================
 var User = require('./models/User'),
     Clients = require('./models/Client');
 
 // =======================================================================================================================================
-//  Include and register all Routes
-// 1. User
-// 2. Clients
+//  Include all Routes But they will be called at the very end
+//    1. User
+//    2. Clients
 //=========================================================================================================================================
-var clientsRoutes = require('./routes/clients');
-var indexRoutes = require('./routes/index');
+var clientsRoutes = require('./routes/clients'),
+    indexRoutes = require('./routes/index');
 
 
 // =======================================================================================================================================
-//  App Cpnfig
+//  App Config
 // 1. Connect To the Required Instance Of MongoDB
 // 2.  Set up other needed configs such as 
-//    a)view engine to avoid continously writing ejs on our views
-//    b) enabling body parser to enable the collection of data from submitted forms
-//    c) Initialize Passport  for authentication
+//    a) View engine to avoid continously writing ejs on our views
+//    b) Enabling body parser to enable the collection of data from submitted forms
+//    c) Serving the Public directory to access it just like the views folder(The public dire)
+//    d) Initialize Passport  for authentication
 //=========================================================================================================================================
 
 mongoose.connect('mongodb://localhost:27017/MolowehouDB', {
@@ -51,18 +52,16 @@ mongoose.connect('mongodb://localhost:27017/MolowehouDB', {
 //     client.close();
 // });
 
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
-app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 
 // Passport Config
-
 app.use(require("express-session")({
     secret: 'Molowehou',
     resave: false,
@@ -77,8 +76,11 @@ passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-//MiddleWare to ensure that the routes are all sent with the logged in User Data
+// ============================================================================================================================================
+//Function to ensure that all the routes are sent with: 
+//     1. the logged in User Data
+//     2. 
+// ============================================================================================================================================
 app.use(function (req, res, next) {
     res.locals.currentUser = req.user;
     next();
@@ -86,28 +88,9 @@ app.use(function (req, res, next) {
 
 
 
-// ==========================================================================================================================================
-//  App Cpnfig
-
-// 1. Define The Schema For the Objects Basically how the object is structured
-// 2. Tie the schema to the Model (The database)
-//==========================================================================================================================================
-
-
-// var CustomerSchema = new mongoose.Schema({
-//     name: String,
-//     age: Number
-// });
-
-// var Customer = mongoose.model('Customer', CustomerSchema);
-
-
-
-
-
-// ========================================================================
+// ===========================================================================================================================================
 //  Secret Page To Test The effectiveness of User Authentication
-//=========================================================================
+//============================================================================================================================================
 app.get('/secret', isLoggedin, function (req, res) {
     res.render('secret');
 });
@@ -131,8 +114,10 @@ function isLoggedin(req, res, next) {
 }
 
 
-
-
+//============================================================================================================================================
+//The Routes Have to be called last, otherwise they will not work
+//They are however declared at the very top
+//============================================================================================================================================
 
 app.use(clientsRoutes);
 app.use(indexRoutes);
